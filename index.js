4490d8fa1812aefa14f67981ae3170c018703529
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-15 11:57:50 -0700

    commit tree 22634abce167ad48d94f8618dc2799df8ed7c565

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString){
+  const parseTime = parseInt(timeString.split(':')[0])
+  if (parseTime < 12){
+  return "Good Morning"
+  } else if (parseTime > 17){
+    return "Good Evening"
+  } else {
+    return "Good Afternoon"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message){
+  const greeting = document.querySelector('#greeting')
+  greeting.innerText = message
+}
